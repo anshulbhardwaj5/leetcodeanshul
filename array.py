5738f0f730fe416172f7647f342c4b6ff92471c6
anshulbhardwaj5 <anshulsharma5199@gmail.com>
2023-08-17 23:06:33 +0530

    array updt

--- array.py
+++ array.py
@@ -15,4 +15,4 @@ b = arr.array('d', [2.5, 3.2, 3.3])
 # printing original array
 print("\nThe new created array is : ", end=" ")
 for i in range(0, 3):
-    print(b[i], end=" ")
+   print(b[i], end=" ")
